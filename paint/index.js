2433c3a2409c4e4116dcb5e67493fb2352223ffc
Muhammad Haroon <84071543+26haroon26@users.noreply.github.com>
2022-08-10 05:47:14 -0700

    Add files via upload

--- paint/index.js
+++ paint/index.js
@@ -8,72 +8,131 @@ let ctx = canva.getContext("2d");
 var strDataURI = canva.toDataURL();
 
 let isMouseDown = false;
-let startx ,
-  starty ,
-  endx ,
-  endy ;
+let startx, starty, endx, endy;
+
+/// for line
 
 // canva.addEventListener("mousedown", (event) => {
-//   isMouseDown = true;
-//   startx = event.offsetX;
-//   starty = event.offsetY;
+//     isMouseDown = true;
 // });
 // canva.addEventListener("mouseup", (event) => {
-//   isMouseDown = false;
+//     ctx.beginPath();
+//     isMouseDown = false;
+// });
+
+// canva.addEventListener("mousemove", (event) => {
+//     if (isMouseDown) {
+//     ctx.lineCap = "round";
+//     ctx.lineWidth = size.value;
+//     ctx.lineTo(event.offsetX, event.offsetY);
+//     ctx.stroke();
+//     // ctx.beginPath();
+//     ctx.moveTo(event.offsetX, event.offsetY)
+//     ctx.strokeStyle = color.value;
+//     }
+// });
+
+// for circle
+
+// canva.addEventListener("mousedown", (event) => {
+//   startx = event.offsetX;
+//   starty = event.offsetY;
+//   isMouseDown = true;
+// });
+// canva.addEventListener("mouseup", (event) => {
 //   endx = event.offsetX;
 //   endy = event.offsetY;
-// });
-// canva.addEventListener("mousemove", (event) => {
-//   if (isMouseDown) {
-//     // console.log(startx, starty, endx, endy);
-//     // ctx.moveTo(startx, starty);
-//     // ctx.lineTo(endx, endy);
-//     // ctx.stroke();
-
-//     ctx.fillStyle = color.value;
-//     ctx.fillRect(event.offsetX, event.offsetY, size.value, size.value);
-//   }
+//   isMouseDown = false;
+//   ctx.beginPath();
+//     ctx.strokeStyle = color.value;
+// ctx.lineWidth = size.value;
+//   ctx.arc(startx, starty, event.offsetY - starty , 0, 2 * Math.PI);
+//   ctx.stroke();
 // });
 
 canva.addEventListener("mousedown", (event) => {
   startx = event.offsetX;
   starty = event.offsetY;
+  isMouseDown = true;
+  console.log(event);
 });
+// console.log(endy - starty);
+// ctx.restore();
+// ctx.strokeRect(startx, starty, endx, endy);
 canva.addEventListener("mouseup", (event) => {
   endx = event.offsetX;
   endy = event.offsetY;
+  isMouseDown = false;
 
-  
+  ctx.strokeStyle = color.value;
+  ctx.lineWidth = size.value;
   ctx.strokeRect(startx, starty, endx, endy);
   ctx.stroke();
+  // ctx.save();
 });
 startx = "";
 starty = "";
 endx = "";
 endy = "";
+
+function getid(a) {
+  value = `${a}`;
+  console.log(value);
+}
+
+//   canva.addEventListener("mousedown", (event) => {
+//     console.log(event);
+//     startx = event.offsetX;
+//     starty = event.offsetY;
+
+//     isMouseDown = true;
+//     // ctxCopy = JSON.stringify(ctx);
+// });
+// canva.addEventListener("mouseup", (event) => {
+//   endx = event.offsetX;
+//   endy = event.offsetY;
+//   isMouseDown = false;
+
+//   // ctx.strokeRect(startx, starty, endx, endy);
+//   // ctx.stroke();
+// });
+
+// canva.addEventListener('mousemove', (event) => {
+//     if (isMouseDown) {
+
+//       // ctx = JSON.parse(ctxCopy)
+//         ctx.fillRect(event.offsetX, event.offsetY, size.value, size.value);
+//         // ctx.strokeRect(startx, starty, endx, endy);
+//         ctx.fillStyle = color.value;
+//         ctx.stroke();
+//     }
+
+// });
+
 function forReset() {
-      ctx.clearRect(0, 0, canva.width, canva.height);
-      var w = canva.width;
-      canva.width = 0;
-      canva.width = w;
-    }
-    function forSave() {
-          console.log(canva.toDataURL());
-          document.getElementById("savedImage").href = `${canva.toDataURL()}`;
-        }
-        
-        // // var canvas = document.getElementById("myCanvas");
-        // // var ctx = canvas.getContext("2d");
-        // // ctx.moveTo(0, 0);
-        // // ctx.lineTo(200, 100);
-        // // ctx.stroke();
-        
-        // vacanvas = document.getElementById("myCanvas");
-        // var ctx = canvas.getContext("2d");
-        // ctx.beginPath();
-        // ctx.arc(55, 50, 40, 0, 2 * Math.PI);
-        // ctx.stroke();
-        
-        
-        //for circle
-        //   ctx.arc(startx, starty, 40, 0, 2 * Math.PI);
+  ctx.clearRect(0, 0, canva.width, canva.height);
+  var w = canva.width;
+  canva.width = 0;
+  canva.width = w;
+}
+function forSave() {
+  let canvas = document.querySelector("#myCanvas");
+  let anchor = document.createElement("a");
+  anchor.href = canvas.toDataURL("image/jpg");
+  anchor.download = "paint.jpg";
+  anchor.click();
+}
+
+// // var canvas = document.getElementById("myCanvas");
+// // var ctx = canvas.getContext("2d");
+// // ctx.moveTo(0, 0);
+// // ctx.lineTo(200, 100);
+// // ctx.stroke();
+
+// vacanvas = document.getElementById("myCanvas");
+// var ctx = canvas.getContext("2d");
+// ctx.beginPath();
+// ctx.arc(55, 50, 40, 0, 2 * Math.PI);
+// ctx.stroke();
+
+//for circle
